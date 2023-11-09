b35925f401419011b66cb45fd7848bf2cdcc453d
Felipe24121990 <147284126+Felipe24121990@users.noreply.github.com>
2023-11-09 13:54:26 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário

--- banco de dados com delete.cpp
+++ banco de dados com delete.cpp
@@ -127,9 +127,10 @@ int main()
 	
 	    printf("### Cartório da EBAC ###\n\n"); //Inicio do menu 
 	    printf("Escolha a opção desejada do menu:\n\n");
-	    printf("opcao \t1 - Registrar nomes\n");
-	    printf("opcao \t2 - Consultar nomes\n");
-	    printf("opcao \t3 - Deletar nomes\n\n"); 
+	    printf("\t1 - Registrar nomes\n");
+	    printf("\t2 - Consultar nomes\n");
+	    printf("\t3 - Deletar nomes\n"); 
+	    printf("\t4 - Sair do sistema\n\n");
      	printf("Escolha a opcao desejada: "); //fim do menu 
 	
       	scanf("%d", &opcao); //armazenando a escolha do usuário  
@@ -148,6 +149,11 @@ int main()
 	   
        	    case 3: 
 	        deletar();
+	        break;
+	        
+	        case 4:
+	        printf("Obrigado por utilizar o sistema!\n");
+	        return 0;
 	        break;
 	   
 	        default:
